59b2e86c984da93d374af9f0d6f12e823d5a1127
longwatermelon <longwaddermelon@gmail.com>
2021-11-24 13:35:04 -0700

    refactor

--- src/point.c
+++ src/point.c
@@ -3,21 +3,12 @@
 #include <SDL2/SDL_render.h>
 
 
-void point_render(struct Point* point, SDL_Renderer* rend)
-{
-    struct Vec2f proj = point_project(point);
-
-    SDL_SetRenderDrawColor(rend, 255, 255, 0, 255);
-    SDL_RenderDrawPoint(rend, (int)proj.x, (int)proj.y);
-}
-
-
-struct Vec2f point_project(struct Point* orig)
+SDL_FPoint point_project(struct Point* orig)
 {
     if (orig->z <= 0.f)
-        return (struct Vec2f){ .x = -1.f, .y = -1.f };
+        return (SDL_FPoint){ .x = -1.f, .y = -1.f };
 
-    struct Vec2f proj = {
+    SDL_FPoint proj = {
         .x = orig->x / orig->z + 1,
         .y = orig->y / orig->z + 1
     };
@@ -31,13 +22,13 @@ struct Vec2f point_project(struct Point* orig)
 
 void point_draw_trail(struct Point* point, SDL_Renderer* rend)
 {
-    struct Vec2f front = point_project(point);
+    SDL_FPoint front = point_project(point);
 
     if (front.x == -1.f && front.y == -1.f)
         return;
 
     struct Point p = { .x = point->x, .y = point->y, .z = point->z - point->length };
-    struct Vec2f back = point_project(&p);
+    SDL_FPoint back = point_project(&p);
 
     if (back.x == -1.f && back.y == -1.f)
         return;
--- src/point.h
+++ src/point.h
@@ -3,19 +3,13 @@
 
 #include <SDL2/SDL.h>
 
-struct Vec2f
-{
-    float x, y;
-};
-
 struct Point
 {
     float x, y, z;
     float length;
 };
 
-void point_render(struct Point* point, SDL_Renderer* rend);
-struct Vec2f point_project(struct Point* orig);
+SDL_FPoint point_project(struct Point* orig);
 void point_draw_trail(struct Point* point, SDL_Renderer* rend);
 
 #endif
